ad51c669c0eb7c5903018d430b78dbf1aa7d5691
hiderikzki <foreheadchann@googlemail.com>
2024-05-31 15:07:39 +0300

    why are these here still ?

--- PixelGunCheat/Cheat/Hooks/Detour/DetourVars.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <vector>
-
-class DetourVars
-{
-public:
-    static inline std::vector<void*> player_list = {};
-    static inline std::vector<void*> working_player_list = {};
-};
